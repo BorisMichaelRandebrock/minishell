8b77ae70b59e9e98e1e7562ce642f3acb2f9faaa
Lineath <fran.montserrat.developer@gmail.com>
2024-02-12 20:09:14 +0100

    masked split research

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: fmontser <fmontser@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:34:08 by brandebr          #+#    #+#             */
-/*   Updated: 2024/02/12 14:20:47 by fmontser         ###   ########.fr       */
+/*   Updated: 2024/02/12 20:07:32 by fmontser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 #include <stdbool.h>
 #include "minishell.h"
 #include "libft.h"
+
+#define SPC_CH ' '
+#define TAB_CH '\t'
+#define SQU_CH '\''
+#define DQU_CH '"'
+#define MSQ_SPC 0
 
 #define N_TOKEN_TYPES 9
 
@@ -31,70 +37,34 @@ static void	_destructor(t_parser *parser)
 }
 
 
-static void	_get_term_list(t_parser *parser, t_prompt *prompt, t_token *head_token)
-{
-	int	i;
-
-	parser->_split = ft_split(prompt->_input, ' ');
-	i = 0;
-	head_token = new_token();
-	head_token->text = parser->_split[i];
-	head_token->type = head_token->get_type(parser->_split[i++]);
-	while(parser->_split[i])
-	{
-		head_token->next = new_token();
-		head_token->next->prev = head_token;
-		head_token->next->text = parser->_split[i];
-		head_token->next->type = head_token->get_type(parser->_split[i++]);
-		head_token = head_token->next;
-	}
-	while(i-- > 0)
-	{
-		//TODO NO podemos determinar si es un CMD sin compararlo con los nombres de los comandos disponibles en:
-		//	- comandos residentes en ruta absoluta
-		//	- comandos residentes en ruta relativa
-		//	- comandos residentes en PATH
-		// Existe este termino en este directorio?
-		if (head_token->type == TEXT && (!head_token->prev || head_token->prev->type != TEXT))
-			head_token->type = CMD;
-		else if (head_token->type == TEXT)
-			head_token->type = ARG;
-		if (head_token->prev)
-			head_token = head_token->prev;
-	}
-}
-
-static bool	_validate_input(t_token *token_lst)
-{
-	t_token *head;
-	int	counter[N_TOKEN_TYPES];
-	//TODO blockeado mientras no resolvemos CMD (identificarlo)
-	head = token_lst;
-	while (head->next)
-	{
-		counter[head->type]++;
-		if (head->type == PIPE && (!head->next || !head->prev))
-			return (false);
-		head = head->next;
-	}
-	if (counter[CMD] == 0)
-		return (false);
-	return (true);
-}
-
 //Parse user input via prompt object
 static void	_parse_prompt(t_prompt *prompt)
 {
-	t_token	*token_lst;
+	(void) prompt;
+	char	*test = "echo \"hola| adios\" si \"seny   or\"cat -e";
+	(void) test;
+/* masked_split
 
+1. sacar las mascaras con retro-marcado
 
-	token_lst = NULL;
-	_get_term_list(prompt->_shell->_parser, prompt, token_lst);
-	if (!_validate_input(token_lst))
-	{
-		printf("error!");
-		return ;
-	}
+						echo "hola|   que" 'tal'"tio"    '  | cat -e
+"mask					00000011111111111000000001110000000000000000
+'mask					00000000000000000000111000000000000000000000
+ch_mask					11110111111000111101111111111000010010111011
+
+2. recorrer y guardar en el heap (char **), no necesita numero previo.
+
+mode ch
+						echo
+mode "	(" & ch)			 hola|   que
+mode '	(' & ch)							tal
+mode "  (" & ch)							     tio
+mode ch													 '
+mode op (!(" & ') & ch)										|
+mode ch														  cat -e
+
+3. Integrar el array en el parser. */
+
 }
 
 //Create new process object
