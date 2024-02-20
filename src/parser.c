bb69dcbf9cbb865a515aaa32b682e5d9126b26a4
Lineath <fran.montserrat.developer@gmail.com>
2024-02-21 00:23:59 +0100

    initial hotfix commit

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: fmontser <fmontser@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:34:08 by brandebr          #+#    #+#             */
-/*   Updated: 2024/02/21 00:08:13 by fmontser         ###   ########.fr       */
+/*   Updated: 2024/02/21 00:22:55 by fmontser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,8 @@ void	parse(char *raw)
 	int		i;
 	bool	flag;
 
+
+	//TODO @@@@@@@@@@@@ fix parser!!
 	flag = true;
 	i = 0;
 	while (raw[i])
@@ -85,3 +87,65 @@ void	parse(char *raw)
 		i++;
 	}
 }
+
+
+/*
+static t_token	*_extract_token(char *raw, int start, int end)
+{
+	t_token	*tkn;
+	size_t	length;
+	char	*substr;
+
+	length = ++end - start;
+	tkn = sh_calloc(NULL, sizeof(t_token), STRCT);
+	substr = ft_substr(raw, start, length);
+	tkn->string = ft_strtrim(substr, " ");
+	free(substr);
+	//TODO guardar el puntero en lista CNT
+	return (tkn);
+}
+
+void	parse(t_shell *sh, char *raw)
+{
+	int		i;
+	int		start;
+	int		end;
+	bool	flag;
+	char	dlmt;
+
+	char *debug = raw;
+
+	flag = true;
+	i = 0;
+	start = 0;
+	dlmt = SPC_CH;
+	while (raw[i])
+	{
+		if (raw[i] == SPC_CH && !flag)
+		{
+			i++;
+			debug++;
+			continue ;
+		}
+		if (!flag)
+		{
+			if (raw[i] == SQU_CH || raw[i] == DQU_CH)
+				dlmt = raw[i];
+			else
+				dlmt = SPC_CH;
+			flag = true;
+			start = i;
+		}
+		else if ((raw[i] == dlmt || raw[i + 1] == NUL_CH) && flag)
+		{
+			flag = false;
+			end = i;
+			if (!sh->tkn_lst)
+				sh->tkn_lst = ft_lstnew(_extract_token(raw, start, end));
+			else
+				ft_lstadd_back(&sh->tkn_lst, ft_lstnew(_extract_token(raw, start, end)));
+		}
+		i++;
+		debug++;
+	}
+}*/
