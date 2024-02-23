70d9b4929f9357f5aa91d4e264b100771095a087
Boris Randebrock <brandebr@car3s2.42barcelona.com>
2024-02-23 18:58:46 +0100

    typifyng & cleaning tokens

--- src/minishell.c
+++ src/minishell.c
@@ -3,47 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmontser <fmontser@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: brandebr <brandebr@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 18:19:28 by fmontser          #+#    #+#             */
-/*   Updated: 2024/02/22 19:39:09 by fmontser         ###   ########.fr       */
+/*   Updated: 2024/02/23 18:13:13 by brandebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
 #include "libft.h"
+#include "minishell.h"
 #include <readline/history.h>
 #include <readline/readline.h>
 
-t_shell	*get_shell()
+t_shell	*get_shell(void)
 {
-	static	t_shell *shell = NULL;
+	static t_shell	*shell = NULL;
 
 	if (!shell)
 		shell = ft_calloc(1, sizeof(t_shell));
-		if (!shell)
-			sh_perror(MEM_ERROR);
+	if (!shell)
+		sh_perror(MEM_ERROR);
 	return (shell);
 }
 
 int	main(int argc, char *argv[], char *env[])
 {
-	(void)argc;
-	(void)argv;
 	t_shell	*sh;
 
+	(void)argc;
+	(void)argv;
 	sh = new_sh(env);
- 	while(sh->is_running)
+	while (sh->is_running)
 	{
-		//sh->raw = sh_addfree(ft_strdup("echo \"hola\" 'adios"));
-		sh->raw = sh_addfree(readline("🐌 minishell> "));
+		sh->raw = sh_addfree(ft_strdup("'echo' |\"hola\">>'adios'"));
+		// sh->raw = sh_addfree(readline("🐌 minishell> "));
 		parse(sh->raw);
-
-
 		while (sh->tkn_lst)
 		{
-		printf("%s\n", ((t_token *)sh->tkn_lst->content)->string);
-		sh->tkn_lst = sh->tkn_lst->next;
+			printf("%s\n", ((t_token *)sh->tkn_lst->content)->string);
+			sh->tkn_lst = sh->tkn_lst->next;
 		}
 		break ;
 	}
--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: brandebr <brandebr@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:34:08 by brandebr          #+#    #+#             */
-/*   Updated: 2024/02/23 17:26:50 by brandebr         ###   ########.fr       */
+/*   Updated: 2024/02/23 18:10:55 by brandebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,62 @@
 
 #define WHSPC_CHRS " \t\n\r\f\v"
 
-static void	_expand_vars(char *tkn_str)
+// static void	_expand_vars(char *tkn_str)
+// {
+// 	// TODO expandir las varaibles
+// }
+bool	_is_op(char *str)
 {
-	// TODO expandir las varaibles
+	if ((*str == '<' || *str == '>') && *str == *(str + 1))
+		return (1);
+	if (*str == '<' || *str == '>' || *str == '|')
+		return (1);
+	return (0);
 }
+static void	_clean_token(t_token *tkn)
+{
+	t_shell	*sh;
+	t_list	*tmp;
 
+	sh = get_shell();
+	tmp = sh->tkn_lst;
+	if (tkn->type == ARG || tkn->type == CMD)
+	{
+		if (tkn->string[0] == DQU_CH || tkn->string[0] == SQU_CH)
+			tkn->string++;
+		if (tkn->string[ft_strlen(tkn->string) - 1] == DQU_CH
+			|| tkn->string[ft_strlen(tkn->string) - 1] == SQU_CH)
+			tkn->string[ft_strlen(tkn->string) - 1] = NUL_CH;
+	}
+	// TODO limpiar las comillas
+}
 static void	_typify_token(t_token *tkn)
 {
 	t_shell	*sh;
-	t_token	*prev_tkn;
+	t_list	*prev_tkn;
 	t_list	*_tkn_lst;
 
-	_tkn_lst = sh->tkn_lst;
 	prev_tkn = NULL;
 	sh = get_shell();
-	while (!_tkn_lst->next->content == tkn)
+	_tkn_lst = sh->tkn_lst;
+	// while (!_tkn_lst->next->content == tkn)
+	// 	_tkn_lst = _tkn_lst->next;
+	// prev_tkn = sh->tkn_lst;
+	while (_tkn_lst)
+	{
+		if (prev_tkn == NULL || (*(t_token *)prev_tkn->content).type == '|')
+			tkn->type = CMD;
+		else if (_is_op(tkn->string))
+			tkn->type = OP;
+		else
+			tkn->type = ARG;
+		prev_tkn = _tkn_lst;
 		_tkn_lst = _tkn_lst->next;
-	prev_tkn = sh->tkn_lst;
-	if (_is_op(tkn->string))
-		tkn->type = OP;
-	else if (_is_cmd(tkn->string))
-		tkn->type = CMD;
-	else
-		tkn->type = ARG;
-	// TODO tipificar los token, y limpiar las comillas
-	// cmd: primero o    primero despues de |.
-	// args: empeiza y acaban en quotes, no son primeros,
-	pero si cuando le precede un redireccionador.
+		// TODO tipificar los token, y limpiar las comillas
+		// cmd: primero o    primero despues de |.
+		// args: empeiza y acaban en quotes, no son primeros,
+		// pero si cuando le precede un redireccionador.
+	}
 }
 
 static void	_extract_token(char *start, char *end)
@@ -81,6 +110,7 @@ void	_extract_op(char *raw)
 	substr = sh_addfree(ft_substr(raw, 0, op_sz));
 	tkn->string = substr;
 	_typify_token(tkn);
+	_clean_token(tkn);
 	tmp = sh_addfree(ft_lstnew(tkn));
 	if (!sh->tkn_lst)
 		sh->tkn_lst = tmp;
