aa78e59ea62397ed1ca656053bb95ad841c79015
Lineath <fran.montserrat.developer@gmail.com>
2024-03-07 16:34:01 +0100

    exit complete

--- src/__builtin/__cd.c
+++ src/__builtin/__cd.c
@@ -1,38 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   __env.c                                            :+:      :+:    :+:   */
+/*   __cd.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmontser <fmontser@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/01 19:20:14 by fmontser          #+#    #+#             */
-/*   Updated: 2024/03/07 16:11:20 by fmontser         ###   ########.fr       */
+/*   Created: 2024/03/07 16:26:03 by fmontser          #+#    #+#             */
+/*   Updated: 2024/03/07 16:26:11 by fmontser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include "minishell.h"
 
-int	__env(t_list *args, int fd)
+int	__cd(t_list *args, int fd)
 {
-	t_shell	*sh;
-	size_t	i;
-
 	(void)args;
-	sh = get_shell();
-	i = 0;
-	while (i < sh->env_sz)
-	{
-		write(fd, sh->env[i], ft_strlen(sh->env[i]));
-		if (++i < sh->env_sz)
-			write(fd, "\n", 1);
-	}
-	if (fd == STDOUT_FILENO)
-		write(fd, "\n", 1);
-	if (fd > SYS_FDS)
-	{
-		write(fd, "\0", 1);
-		close(fd);
-	}
-	return (SUCCESS);
+	(void)fd;
+	return (0);
 }
--- src/__builtin/__exit.c
+++ src/__builtin/__exit.c
@@ -1,38 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   __env.c                                            :+:      :+:    :+:   */
+/*   __exit.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmontser <fmontser@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/01 19:20:14 by fmontser          #+#    #+#             */
-/*   Updated: 2024/03/07 16:11:20 by fmontser         ###   ########.fr       */
+/*   Created: 2024/03/07 16:16:31 by fmontser          #+#    #+#             */
+/*   Updated: 2024/03/07 16:33:11 by fmontser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include "minishell.h"
 
-int	__env(t_list *args, int fd)
+int	__exit(t_list *args, int fd)
 {
-	t_shell	*sh;
-	size_t	i;
-
 	(void)args;
-	sh = get_shell();
-	i = 0;
-	while (i < sh->env_sz)
-	{
-		write(fd, sh->env[i], ft_strlen(sh->env[i]));
-		if (++i < sh->env_sz)
-			write(fd, "\n", 1);
-	}
-	if (fd == STDOUT_FILENO)
-		write(fd, "\n", 1);
-	if (fd > SYS_FDS)
-	{
-		write(fd, "\0", 1);
-		close(fd);
-	}
+	(void)fd;
+	sh_exit(SUCCESS);
 	return (SUCCESS);
 }
