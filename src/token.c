c6e20117e62c709e9b7424c882aafdd8cfa95e7f
Lineath <fran.montserrat.developer@gmail.com>
2024-04-09 23:27:32 +0200

    minor fix

--- src/token.c
+++ src/token.c
@@ -6,13 +6,14 @@
 /*   By: fmontser <fmontser@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:34:08 by brandebr          #+#    #+#             */
-/*   Updated: 2024/04/09 17:04:58 by fmontser         ###   ########.fr       */
+/*   Updated: 2024/04/09 23:27:11 by fmontser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-#define WHSPC_CHRS " \t\n\r\f\v"
+#define WHSPC_CHRS	" \t\n\r\f\v"
+#define DLMT_CHRS	"\"\'><|\0"
 
 static void	_pre_typify(t_token *tkn)
 {
@@ -43,15 +44,27 @@ static void	_extract_token(char *start, char *end)
 
 	sh = get_shell();
 	length = (end + 1) - start;
+	if (end == start)
+		length = 1;
 	tkn = sh_calloc(1, sizeof(t_token));
 	substr = sh_guard(ft_substr(start, 0, length), NULL);
 	tkn->str = sh_guard(ft_strtrim(substr, WHSPC_CHRS), substr);
 	_pre_typify(tkn);
-	token_expansion(tkn);
+	if (tkn->type == ARG)
+		token_expansion(tkn);
 	ft_lstadd_back(&sh->tknlst, sh_guard(ft_lstnew(tkn), NULL));
 }
 
+static bool	is_operator(char c)
+{
+	if (c == '>' || c == '<' || c == '|')
+		return (true);
+	return (false);
+}
+
 // Parse a input prompt into a token list
+
+//TODO cacafruti total....idea funcion extract con target end!!
 void	tokenizer(char *input)
 {
 	char	*_input;
@@ -67,10 +80,22 @@ void	tokenizer(char *input)
 		if (*_input == '"' || *_input == '\'')
 			dlmt = *_input;
 		tkn_start = _input;
-		while (*(++_input) != dlmt && *_input)
-			;
+		while ((*(++_input) != dlmt && *_input))
+		{
+			if (is_operator(*_input) && dlmt == ' ')
+				break ;
+		}
 		_extract_token(tkn_start, _input);
-		_input++;
 	}
 	sh_free(&input);
 }
+
+/*
+if (is_operator(*_input))
+{
+	tkn_start = _input;
+	if (*_input == *(_input + 1) && *_input != '|')
+		_input++;
+	_extract_token(tkn_start, _input);
+}
+ */
